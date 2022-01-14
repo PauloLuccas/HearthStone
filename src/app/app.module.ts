bfcb79a1d5886a76e0587c69c88271501b93bf2b
PauloLuccas <paulolucas.dev@gmail.com>
2022-01-14 16:29:53 -0300

    criando componente dos cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
+import { CardsComponent } from './section/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SectionComponent
+    SectionComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
