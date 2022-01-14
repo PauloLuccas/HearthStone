a5454e16cba3729f208b504b75b9e048dbc7fcdd
PauloLuccas <paulolucas.dev@gmail.com>
2022-01-14 15:37:59 -0300

    criando local storage serve e armazenando dados.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
+
+const cardKey = 'card_key'
 
 @Component({
   selector: 'app-header',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public card: any = {};
+
+  onSubmit(form: any) {
+    console.log(form);
+    console.log(this.card);
+    this.salveList()
+  }
+
+  constructor(private storageService: LocalStorageService) {
+  }
+
+  salveList() {
+    console.log(cardKey, this.card)
+    this.storageService.set(cardKey, this.card)
+  }
 
   ngOnInit(): void {
   }
