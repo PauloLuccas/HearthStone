426d1802e629e02532dcf0f5f2e29f8b64c116c8
PauloLuccas <paulolucas.dev@gmail.com>
2022-01-14 16:03:48 -0300

    ajusatando o array que esta sendo salvo no local storage e resetando o form apos o envio.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,18 +12,39 @@ export class HeaderComponent implements OnInit {
 
   public card: any = {};
 
+  public cardList: any = [];
+
   onSubmit(form: any) {
     console.log(form);
     console.log(this.card);
-    this.salveList()
+    this.addItem(this.card);
   }
 
   constructor(private storageService: LocalStorageService) {
   }
 
   salveList() {
-    console.log(cardKey, this.card)
-    this.storageService.set(cardKey, this.card)
+    console.log(cardKey, this.cardList)
+    this.storageService.set(cardKey, this.cardList)
+  }
+
+
+  addItem(item: object) {
+    this.cardList.push(item);
+    console.log(this.cardList)
+    this.salveList()
+    this.clearForm()
+  }
+
+  clearForm() {
+    console.log(this.card.id)
+    this.card.id = '',
+    this.card.nome = '',
+    this.card.descricao = '',
+    this.card.ataque = '',
+    this.card.defesa = '',
+    this.card.tipo = '',
+    this.card.classe = ''
   }
 
   ngOnInit(): void {
